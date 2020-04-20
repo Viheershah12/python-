2801a40838d8440f5035fbb9c7af49e8e7fe5203
Viheershah12 <63543803+Viheershah12@users.noreply.github.com>
2020-04-20 11:50:22 +0300

    commit

--- courses.py
+++ courses.py
@@ -21,18 +21,32 @@ courses =   [
 #check the items in the user selection against the itenms
 #each courses array and corresponding value obtained as a percentage
 #return the course with the greatest value
+print("select five options from the interests provided below")
 for interest in intersts:
-	for i in interest:
-		print(i,end="\t")
-	print()
- 
-user_interests = []
-opt = 0
-while opt != "":
-    opt = input("Enter your choice (number): ")
-    if opt != "":
-        user_interests.append(opt)
-
+    print(str(interest[0]),'.',interest[1])
+selection = []
+num = 0
+while num < 5:
+    x = num + 1
+    select = int(input("Enter option " + str(x) + ": "))
+    if select != " " and select not in selection:
+        selection.append(selection)
+        num += 1
+print(selection)
+classifier = []
+for course in courses:
+    match = 0
+    interestList = course[0]
+    for userInterest in selection:
+        if userInterest in interestList:
+            match += 1
+    score = match/5
+    result = [course[1],score]
+    classifier.append(result)
+# veiw result
+for course in classifier:
+    print(str(course[1]) + "\t" + course[0])
+input("Enter to Exit")
 
             
 
