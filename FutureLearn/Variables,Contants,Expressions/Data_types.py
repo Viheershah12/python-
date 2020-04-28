f7e46bcdb0fbeeb6d4a54e84a39f6e48dcb5d581
viheer12 <Viheershah12@outlook.com>
2020-04-28 18:33:16 +0300

    fresh commit

---
+++ FutureLearn/Variables,Contants,Expressions/Data_types.py
@@ -0,0 +1,19 @@
+word = "hello"
+phrase = word + " " + "there"
+print(phrase)
+
+number = 5
+# sum = number + "2" =>this would throw a type error
+sum = number + 2
+#this would print 7
+print(sum)
+
+#floating point and integer
+a = float(2)
+b = int(2)
+print(a) #this is a floating point number
+print(b) #this is an integer
+
+#how to make the user input the data he/she would like to enter
+msg  = input("Enter a msg of your choice: ")
+print(msg)
---
+++ FutureLearn/Variables,Contants,Expressions/operators.py
@@ -0,0 +1,24 @@
+a = 5
+a = a+2
+#this would give out 7
+print(a)
+
+a = a-2
+#this would give out 5
+print(a)
+
+a = a*2
+#this would give out 10
+print(a)
+
+a = a/5
+#this would give out 2
+print(a)
+
+a = a**2
+#this would give out 4
+print(a)
+
+a = a%3
+#this would give out 1
+print(a)
